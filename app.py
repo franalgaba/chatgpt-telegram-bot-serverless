b9cead2742e5c18f0625cb215de7f0abdf2b157c
Allen Hebden <allen.hebden@gmail.com>
2023-03-05 11:52:58 -0500

    working with db

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-import os
+import datetime
 import json
+import os
 import traceback
+import uuid
 
+import boto3
 import openai
-from loguru import logger
+from boto3.dynamodb.conditions import Attr, Key
 from chalice import Chalice
-from telegram.ext import (
-    Dispatcher,
-    MessageHandler,
-    Filters,
-    CommandHandler,
-)
-from telegram import ParseMode, Update, Bot
 from chalice.app import Rate
+from loguru import logger
+from telegram import Bot, ParseMode, Update
+from telegram.ext import CommandHandler, Dispatcher, Filters, MessageHandler
 
 from chalicelib.utils import generate_transcription, send_typing_action
 
@@ -32,15 +31,29 @@ app.debug = True
 bot = Bot(token=TOKEN)
 dispatcher = Dispatcher(bot, None, use_context=True)
 
+
+def message_guid():
+    """Generate a random guid for message key"""
+    return str(uuid.uuid4())
+
+
 #####################
 # Telegram Handlers #
 #####################
 
 
-def ask_chatgpt(text):
+def ask_chatgpt(text, old_messages):
+    formatted_old_messages = [
+        {
+            "role": message.get("role"),
+            "content": message.get("text"),
+        }
+        for message in old_messages
+    ]
     message = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
+        messages=formatted_old_messages
+        + [
             {
                 "role": "assistant",
                 "content": text,
@@ -73,23 +86,74 @@ def process_voice_message(update, context):
 
 @send_typing_action
 def process_message(update, context):
+    dynamodb = boto3.resource("dynamodb", region_name="ca-central-1")
+    table = dynamodb.Table("message")
+
     chat_id = update.message.chat_id
+    db_chat_id = f"chat_{chat_id}"
     chat_text = update.message.text
+    created_at = int(datetime.datetime.now().timestamp())
+
+    response = table.scan(FilterExpression=Attr("chat_id").eq(db_chat_id))
+    old_messages = response["Items"]
+
+    if chat_text == "/clear":
+        for message in old_messages:
+            table.delete_item(
+                Key={
+                    "message_key": message["message_key"],
+                    "created_at": message["created_at"],
+                }
+            )
+        context.bot.send_message(
+            chat_id=chat_id,
+            text="Chat cleared",
+            parse_mode=ParseMode.MARKDOWN,
+        )
+        return
+
+    table.put_item(
+        Item={
+            "message_key": message_guid(),
+            "chat_id": db_chat_id,
+            "role": "user",
+            "text": chat_text,
+            "created_at": created_at,
+        }
+    )
 
     try:
-        message = ask_chatgpt(chat_text)
+        message = ask_chatgpt(chat_text, old_messages)
+        context.bot.send_message(
+            chat_id=chat_id,
+            text=message,
+            parse_mode=ParseMode.MARKDOWN,
+        )
     except Exception as e:
         app.log.error(e)
         app.log.error(traceback.format_exc())
         context.bot.send_message(
             chat_id=chat_id,
-            text="There was an exception handling your message :(",
+            text=f"There was an exception handling your message :( {e}",
             parse_mode=ParseMode.MARKDOWN,
         )
-    else:
+
+    try:
+        table.put_item(
+            Item={
+                "message_key": message_guid(),
+                "chat_id": db_chat_id,
+                "role": "assistant",
+                "text": message,
+                "created_at": created_at,
+            }
+        )
+    except Exception as e:
+        app.log.error(e)
+        app.log.error(traceback.format_exc())
         context.bot.send_message(
             chat_id=chat_id,
-            text=message,
+            text=f"There was an exception storing the bot message :( {e}",
             parse_mode=ParseMode.MARKDOWN,
         )
 
@@ -101,7 +165,6 @@ def process_message(update, context):
 
 @app.lambda_function(name=MESSAGE_HANDLER_LAMBDA)
 def message_handler(event, context):
-
     dispatcher.add_handler(MessageHandler(Filters.text, process_message))
     dispatcher.add_handler(MessageHandler(Filters.voice, process_voice_message))
 
